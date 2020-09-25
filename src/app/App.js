cdef4493a4432e15ae0e1536aecdb7a0e7765200
iambenbright <yankeybenneth@gmail.com>
2020-09-25 21:32:49 +0000

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h4">Managed Landing Page</Typography>
+    </Box>
+  );
+}
+
+export default App;
