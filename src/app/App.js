c10349d2d1efe665b6eae4b64709a80a88dc22d4
iambenbright <yankeybenneth@gmail.com>
2020-09-26 03:47:01 +0000

    feat: add landing page

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Typography variant="h4">Managed Landing Page</Typography>
-    </Box>
+    <>
+      <CssBaseline />
+      <Landing />
+    </>
   );
 }
 
