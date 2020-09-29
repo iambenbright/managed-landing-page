a753569d53f1ff6c509a7e2d8d93d27d1278d6b8
iambenbright <yankeybenneth@gmail.com>
2020-09-29 22:00:49 +0000

    feat: add comments sectiion

--- src/app/pages/Landing.js
+++ src/app/pages/Landing.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import Comments from '../components/Comments';
 
 const Landing = () => {
   return (
@@ -10,6 +11,7 @@ const Landing = () => {
       <Navigation />
       <Hero />
       <Services />
+      <Comments />
     </>
   );
 };
