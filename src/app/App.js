3ef7eedc8aa6cbd2fb11ec8b8c14276a7c6115db
iambenbright <yankeybenneth@gmail.com>
2020-10-04 23:48:36 +0000

    feat: customise global theme

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Landing from './pages/Landing';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+const defaultTheme = createMuiTheme();
+
+const theme = createMuiTheme({
+  ...defaultTheme,
+  palette: {
+    primary: {
+      main: deepOrange['500'],
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Landing />
-    </>
+    </ThemeProvider>
   );
 }
 
