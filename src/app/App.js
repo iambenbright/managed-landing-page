b047a1835a0b1fee7deaa408949b4b71962c4173
iambenbright <yankeybenneth@gmail.com>
2020-12-24 00:48:59 +0000

    chore: alter default theme

--- src/app/App.js
+++ src/app/App.js
@@ -12,9 +12,29 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       main: deepOrange['500'],
-      dark: deepOrange['300'],
+      dark: deepOrange['200'],
     },
   },
+  typography: {
+    body1: {
+      color: defaultTheme.palette.grey['600'],
+    },
+    h3: {
+      color: defaultTheme.palette.grey['800'],
+    },
+    h4: {
+      color: defaultTheme.palette.grey['800'],
+      fontWeight: 700,
+    },
+  },
+  shadows: [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '0px 3px 5px -1px rgba(255,87,34,0.2), 0px 5px 8px 0px rgba(255,87,34,0.14), 0px 1px 14px 0px rgba(255,87,34,0.12)',
+  ],
   overrides: {
     MuiContainer: {
       root: {
