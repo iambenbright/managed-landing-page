67a27cc9692599381ac175c40652e824a6b99a6d
iambenbright <yankeybenneth@gmail.com>
2020-12-27 02:28:00 +0000

    feat: adds mobile navigation menu

--- src/app/components/NavBar/index.js
+++ src/app/components/NavBar/index.js
@@ -1,65 +1,117 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 // materialui-components
 import AppBar from '@material-ui/core/AppBar';
 import BrandLogo from '../../shared/BrandLogo';
 import Button from '../../shared/Button';
 import Hidden from '@material-ui/core/Hidden';
+import Box from '@material-ui/core/Box';
+import { Typography, useTheme } from '@material-ui/core';
 
 // custom-components
 import { NestedContainer } from '../../shared/NestedContainer';
+
+// styles
 import { useStyles } from './styles';
-import { useTheme } from '@material-ui/core';
 
 export const NAVBAR_HEIGHT = 120;
+const navs = ['Pricing', 'Product', 'About Us', 'Careers', 'Community'];
+
+const HamburgerAndClose = ({ setMenuOpen, fileURL, altName }) => {
+  const classes = useStyles();
+
+  return (
+    <img
+      onClick={() => setMenuOpen(altName === 'hamburger' ? true : false)}
+      className={classes.menuOpenAndClose}
+      src={fileURL}
+      alt={altName}
+    />
+  );
+};
+
+const NavList = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(null);
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <nav className={classes.nav}>
+      <ul className={classes.list}>
+        {navs.map((nav, idx) => (
+          <li key={idx} className={classes.item}>
+            <a href={`#${nav}`}>
+              <Typography
+                onMouseOver={() => {
+                  setIsFocused(true);
+                  setActiveIndex(idx);
+                }}
+                onMouseLeave={() => setIsFocused(false)}
+                style={{
+                  color:
+                    isFocused && activeIndex === idx
+                      ? theme.palette.grey[600]
+                      : null,
+                }}
+              >
+                {nav}
+              </Typography>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 const NavBar = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [isFocused, setIsFocused] = useState(false);
-  const [activeIndex, setActiveIndex] = useState(null);
-
-  const navs = ['Pricing', 'Product', 'About Us', 'Careers', 'Community'];
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <AppBar elevation={0} color="transparent" position="relative">
-      <NestedContainer
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        height={`${NAVBAR_HEIGHT}px`}
-      >
-        <BrandLogo />
-        <Hidden mdUp>
-          <img src={require('../../assets/images/icon-hamburger.svg')} alt="" />
-        </Hidden>
-        <Hidden smDown>
-          <ul className={classes.navigationList}>
-            {navs.map((nav, idx) => (
-              <li key={idx} className={classes.navigationItem}>
-                <a
-                  href={`#${nav}`}
-                  style={{
-                    color:
-                      isFocused && activeIndex === idx
-                        ? theme.palette.grey['600']
-                        : null,
-                  }}
-                  onMouseOver={() => {
-                    setIsFocused(true);
-                    setActiveIndex(idx);
-                  }}
-                  onMouseLeave={() => setIsFocused(false)}
-                >
-                  {nav}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <Button style={{ marginTop: 0 }}>Get Started</Button>
-        </Hidden>
-      </NestedContainer>
-    </AppBar>
+    <Fragment>
+      {/* mobile navigation and backdrop*/}
+      <Hidden mdUp>
+        {menuOpen && (
+          <Fragment>
+            <NavList />
+            <Box className={classes.backdrop} />
+          </Fragment>
+        )}
+      </Hidden>
+      <AppBar elevation={0} color="transparent" position="relative">
+        <NestedContainer
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          height={`${NAVBAR_HEIGHT}px`}
+        >
+          <BrandLogo />
+          <Hidden mdUp>
+            {!menuOpen && (
+              <HamburgerAndClose
+                setMenuOpen={setMenuOpen}
+                fileURL={require('../../assets/images/icon-hamburger.svg')}
+                altName={'hamburger'}
+              />
+            )}
+            {menuOpen && (
+              <HamburgerAndClose
+                setMenuOpen={setMenuOpen}
+                fileURL={require('../../assets/images/icon-close.svg')}
+                altName={'close'}
+              />
+            )}
+          </Hidden>
+          {/* desktop navigation */}
+          <Hidden smDown>
+            <NavList />
+            <Button style={{ marginTop: 0 }}>Get Started</Button>
+          </Hidden>
+        </NestedContainer>
+      </AppBar>
+    </Fragment>
   );
 };
 
--- src/app/components/NavBar/styles.js
+++ src/app/components/NavBar/styles.js
@@ -1,19 +1,63 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
-  navigationList: {
+export const useStyles = makeStyles(theme => ({
+  nav: {
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      backgroundColor: 'transparent',
+      position: 'absolute',
+      top: 120,
+      zIndex: 100,
+    },
+  },
+  list: {
     margin: 0,
     padding: 0,
-    listStyle: "none",
-    display: "flex",
+    listStyle: 'none',
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      padding: '20px 0',
+      borderRadius: 5,
+      flexDirection: 'column',
+      backgroundColor: 'white',
+      width: '90%',
+      margin: '0 auto',
+      alignItems: 'center',
+    },
   },
-  navigationItem: {
-    "&:not(:last-of-type)": {
+  item: {
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+      padding: '20px 0',
+    },
+    '&:not(:last-of-type)': {
       marginRight: 20,
+      [theme.breakpoints.down('sm')]: {
+        marginRight: 0,
+      },
     },
-    "& a": {
-      textDecoration: "none",
-      color: theme.palette.text.primary,
+    '& a': {
+      textDecoration: 'none',
     },
+    '& p': {
+      color: theme.palette.grey[800],
+      fontSize: '1.2rem',
+      [theme.breakpoints.up('md')]: {
+        fontSize: '1rem',
+      },
+    },
+  },
+  menuOpenAndClose: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  backdrop: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    background:
+      'linear-gradient(to bottom, rgba(0, 0, 0, 0.05) 40%, rgba(0, 0, 0, 0.2) 60%)',
+    zIndex: 99,
   },
 }));
